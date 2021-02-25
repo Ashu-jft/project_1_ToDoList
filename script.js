e3916381da4ba439ffa4211ce5649a739c741755
Ashu Babbar <ashu.babbar@jellyfishtechnologies.com>
2021-02-25 19:11:28 +0530

    created  to-do list with prompt

---
+++ script.js
@@ -0,0 +1,35 @@
+
+console.log("ok");
+const inp=prompt("what do you want to do?")
+while(inp!=='4')
+{
+    const inp=prompt("what do you want to do? 1.Write 2. list 3. delete 4. quit");
+if(inp =='1')
+{
+    let count=0;
+    // document.write(5 + 6);
+    const inp1=prompt("enter element");
+    console.log(inp1 + " added to the list");
+    document.write(inp1);
+
+    
+
+
+}
+else if(inp===2)
+{
+
+}
+else if(inp===3)
+{
+
+
+}
+else if(inp===4)
+{
+
+}
+else{
+
+}
+}
