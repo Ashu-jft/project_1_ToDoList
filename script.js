9923514d5f29074d8bc8f50533e5ee006410e0af
Ashu Babbar <ashu.babbar@jellyfishtechnologies.com>
2021-02-26 12:07:00 +0530

    fixed infite loop

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 
-console.log("ok");
-const inp=prompt("what do you want to do?")
-while(inp!=='4')
+console.log("Welcome to the Basic console Todo project");
+var inps =[];
+// inp=prompt("what do you want to do?")
+let inp=0,ele;
+ while(inp!=='4')
 {
-    const inp=prompt("what do you want to do? 1.Write 2. list 3. delete 4. quit");
+    inp=prompt("what do you want to do? 1.Write 2. list 3. delete 4. quit");
 if(inp =='1')
 {
     let count=0;
-    // document.write(5 + 6);
-    const inp1=prompt("enter element");
-    console.log(inp1 + " added to the list");
-    document.write(inp1);
-
     
+    let i=0;
+    inps[i]=prompt("enter element");
+    console.log(inps[i] + " added to the list");
+    document.write(inps[i]);
+    i++;
 
+    // console.log("########################");
+    // for(var j=0;j<inps.length;j++)
+    // {
+    //     console.log(inps[j] + "\n");
+    // }
+
+    // console.log("$$$$$$$$$$$$$$$$$$$$$$$$$");
+    
+  
 
 }
-else if(inp===2)
+else if(inp=='2')
 {
+    console.log("########################");
+    for(var i=0;i<inps.length;i++)
+    {
+        console.log(inps[i] + "\n");
+    }
 
+    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$");
 }
-else if(inp===3)
+else if(inp==3)
 {
+ele=prompt("which element you want to edit?");
+let val=prompt("enter value");
+inps[ele]=val;
 
 
 }
 else if(inp===4)
 {
-
+    break;
 }
 else{
-
+warn("wrong value input");
 }
 }
